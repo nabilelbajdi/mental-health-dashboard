418330267cbbc503405044445bdbe0bc9cc648d6
nabilelbajdi <113439816+nabilelbajdi@users.noreply.github.com>
2024-10-09 19:35:53 +0200

    Add load_data function to parse Timestamp into Hour, Month, Year, Day

---
+++ part_2/app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+import numpy as np 
+
+# Load the data
+@st.cache_data
+def load_data():
+    df = pd.read_csv("Mental Health Dataset.csv")
+    df["Hour"] = pd.to_datetime(df["Timestamp"]).dt.hour
+    df["Month"] = pd.to_datetime(df["Timestamp"]).dt.month
+    df["Year"] = pd.to_datetime(df["Timestamp"]).dt.year
+    df["Day"] = pd.to_datetime(df["Timestamp"]).dt.day_name()
+    return df
+
+df = load_data()
+
+st.title("Mental Health Dataset")
